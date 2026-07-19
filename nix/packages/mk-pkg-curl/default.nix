5579536da36a90bf101ebc3e190dfb72bdf0b9d7
guihuayufeiguohai <167391457+guihuayufeiguohai@users.noreply.github.com>
2026-07-19 21:34:24 +0800

    Update default.nix

--- nix/packages/mk-pkg-curl/default.nix
+++ nix/packages/mk-pkg-curl/default.nix
@@ -1,13 +1,3 @@
-{
-  pkgs ? import ../../utils/default/pkgs.nix,
-  os ? import ../../utils/default/os.nix,
-  arch ? pkgs.callPackage ../../utils/default/arch.nix { },
-  variant ? import ../../utils/default/variant.nix,
-}:
+{ pkgs, os, arch, variant }:
 
-if os == "ios" then
-  pkgs.pkgsCross.iphone64.curl   # iOS arm64 交叉编译的 curl
-else if os == "macos" then
-  pkgs.curl                       # macOS 原生 curl
-else
-  pkgs.curl                       # 其他平台回退
+pkgs.curl
